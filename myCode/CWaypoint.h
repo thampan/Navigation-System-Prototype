02cc1bac344f8d280988bdbb18cf3b46d6234ea1
Jishnu M Thampan <jishnu.mt@gmail.com>
2019-07-24 21:39:38 +0200

    Update CWaypoint.h

--- myCode/CWaypoint.h
+++ myCode/CWaypoint.h
@@ -1,5 +1,4 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
 ****************************************************************************
 * Filename        : CWAYPOINT.H
 * Author          : Jishnu M Thampan
