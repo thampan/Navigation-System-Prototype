dfb620c8813bfe50b90c0ffdff9014abc34b86bb
Jishnu M Thampan <jishnu.mt@gmail.com>
2019-07-24 21:36:51 +0200

    Update CAttributeExtractor.h

--- myCode/CAttributeExtractor.h
+++ myCode/CAttributeExtractor.h
@@ -1,5 +1,4 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
 ****************************************************************************
 * Filename        : CAttributeExtractor.H
 * Author          : Jishnu M Thampan
